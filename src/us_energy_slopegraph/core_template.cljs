c802a572f13028b637461a4e663157d7fb821226
Arne Brasseur <arne@arnebrasseur.net>
2018-04-26 18:51:30 +0200

    Add template to work with

---
+++ src/us_energy_slopegraph/core_template.cljs
@@ -0,0 +1,46 @@
+(ns us-energy-slopegraph.core
+  (:require [cljsjs.d3]
+            [clojure.string :as string]))
+
+(enable-console-print!)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; data
+
+(def data {})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; helpers
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; draw functions
+
+(defn data-join [parent tag class data]
+  )
+
+(defn draw-header [svg years]
+  )
+
+(defn draw-line [svg data-col-1 data-col-2]
+  )
+
+(defn draw-column [svg data-col index custom-attrs]
+  )
+
+(defn draw-slopegraph [svg data]
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Lifecycle
+
+(defn append-svg []
+  )
+
+(defn remove-svg []
+  )
+
+(defn ^:export main []
+  )
+
+(defn on-js-reload []
+  (main))
